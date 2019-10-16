c601d1fc610c68201a253243c767ebda91770e67
Richard Lee <richlee81192@gmail.com>
2019-10-16 22:09:34 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,12 +17,6 @@ class Waiter
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
- 
-  def waiters
-      meals.map do |meal|
-        meal.waiter
-      end
-  end
   
   def meals 
     Meal.all.select do |meal|
